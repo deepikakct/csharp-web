e0a6e9c4983f8a939e7460b432824c5ce60f4aa1
deepika <deepikakct@gmail.com>
2022-04-28 21:10:22 -0500

    first commit

---
+++ Homework1/Birthday Card generator/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Birthday_Card_generator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Birthday_Card_generator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+       
+        [HttpPost]
+        public IActionResult Index(BirthdayCard birthdayCard)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Confirm", birthdayCard);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ Homework1/Birthday Card generator/Models/BirthdayCard.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Birthday_Card_generator.Models
+{
+    public class BirthdayCard
+    {
+        [Required(ErrorMessage = "Please enter From")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter To")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter Messsage")]
+        public string Message { get; set; }
+    }
+}
