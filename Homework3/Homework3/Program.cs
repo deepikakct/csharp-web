f2e3c2d49e527946bc5d771355a4dae4ee59ed66
deepika <deepikakct@gmail.com>
2022-06-09 19:59:39 -0500

    ini

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,40 @@
+﻿using Homework3.Db;
+using Microsoft.EntityFrameworkCore;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// adding data
+using (var context = new Homework3.Db.ContactsContext())
+{
+    Contact contact = new Contact()
+    {
+        ContactName = "Jack",
+        ContactEmail = "Jack@gmail.com",
+        ContactAge = 33,
+        ContactNotes = "Jack",
+        ContactPhoneNumber = "145623",
+        ContactCreatedDate = DateTime.Now,
+        ContactPhoneType = "Mobile"
+
+    };
+    context.Contacts.Add(contact);
+    context.SaveChanges();
+}
+
+// update data
+using (var context = new Homework3.Db.ContactsContext())
+{
+    var contact = context.Contacts.FirstOrDefault();
+    contact.ContactName = "Jill";
+    context.SaveChanges();
+}
+
+// delete data
+using (var context = new Homework3.Db.ContactsContext())
+{
+    var contact = context.Contacts.Where(a => a.ContactId == 1).FirstOrDefault();
+    context.Contacts.Remove(contact);
+    context.SaveChanges(true);
+}
+    
